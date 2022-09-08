0feabff6c1286fac86c6e8fc51fa1ac45f2239fe
Ivan Maltsev <imaltsev2018i@gmail.com>
2022-09-08 19:55:31 +0300

    3 example

--- Example3/Program.cs
+++ Example3/Program.cs
@@ -3,18 +3,24 @@ int N = Convert.ToInt32(Console.ReadLine());
 Console.Write("Новый массив: ");
 void Random()
 {
-    int k = 0;
     double [] array = new double[N];
     Console.Write("[");
     for (int i = 0; i < N-1; i++)
         {
             array[i]=new Random().Next(1, 100);
             Console.Write($"{array[i]}, ");
-            if (array[i]%2==0) k++;
         }
     array[N-1]=new Random().Next(1, 100);
-    if (array[N-1]%2==0) k++;
+double min = array[0];
+double max = array[0];
+    for (int i = 1; i < N; i++)
+        {            
+            if (array[i]>max) max = array[i];
+            else if (array[i]<min) min = array[i];
+        }
+        double delta = max - min;
+
     Console.WriteLine($"{array[N-1]}]");
-    Console.WriteLine($"В массиве {k} чётных чисел.");
+    Console.WriteLine($"Разница между максимальным и минимальным элеменами массива = {delta}");
 }
 Random();
